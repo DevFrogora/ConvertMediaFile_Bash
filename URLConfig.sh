659e7c1f1958bbd252bb59d88dbe17c751b520ff
Frogora <44252430+DevFrogora@users.noreply.github.com>
2022-09-01 18:50:41 +0100

    Update URLConfig.sh

--- URLConfig.sh
+++ URLConfig.sh
@@ -1,3 +1,6 @@
 ScriptDir="Scripts"
 HomeUrl="https://raw.githubusercontent.com/DevFrogora/ConvertMediaFile_Bash/main/Home.sh"
 HomePath="./"$ScriptDir"/Home.sh"
+
+FileVersion=1662052212;
+FileVersionUrl="https://raw.githubusercontent.com/DevFrogora/ConvertMediaFile_Bash/main/CurrentVersion.txt";
