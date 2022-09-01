e7a32b5eb0760bb50ce35287f60af4acc578b072
Frogora <44252430+DevFrogora@users.noreply.github.com>
2022-09-01 17:35:21 +0100

    Update Home.sh

--- Home.sh
+++ Home.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "We are in home";
+echo "-----Convert Media Home------";
+while true;
+do 
+    echo "Choose the Option (Select the number)"
+    echo "1) Convert gif to frames"
+    echo "2) Convert mp4 to frames"
+    read option
+
+    case "$option" in 
+            1) echo "one" ;;
+            2) echo "two" ;;
+            *) echo "default" ;;
+    esac
+done
