76849f95d4fb500c2ceaf9c201ec5403d0cc84e2
DevFrogora <rr4428310@gmail.com>
2022-09-02 19:27:22 +0100

    UpDate

--- Scripts/Home.sh
+++ Scripts/Home.sh
@@ -3,17 +3,18 @@
 . ./Scripts/Config/URLConfig.sh
 . $addonsDir/gif2frames.sh
 
-echo "-----Convert Media Home------";
-while true;
-do 
+echo "-----Convert Media Home------"
+while true; do
     echo "Choose the Option (Select the number)"
     echo "1) Convert gif to frames"
     echo "2) Convert mp4 to frames"
+    echo "3) Report"
     read option
 
-    case "$option" in 
-            1) MakeGifToFrames ;;
-            2) echo "two" ;;
-            *) exit ;;
+    case "$option" in
+    1) MakeGifToFrames ;;
+    2) echo "two" ;;
+    3) $(powershell.exe Start chrome.exe "https://github.com/DevFrogora/ConvertMediaFile_Bash/issues") ;;
+    *) exit ;;
     esac
 done
