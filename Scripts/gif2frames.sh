fe7c0c1460c9585ae19e4a6dd7a6ee3c27d3346c
Frogora <44252430+DevFrogora@users.noreply.github.com>
2022-09-02 08:26:17 +0100

    Update gif2frames.sh

--- Scripts/gif2frames.sh
+++ Scripts/gif2frames.sh
@@ -1,34 +1,30 @@
 #/!bin/bash
 
+. ./Utils.sh
+
 FrameDirectory="Frames"
 NullFile="NUL"
 
 function MakeGifToFrames() {
     echo -e "Pass the File Name and Output_Directory: \c"
     read filename outputDir
-    #filename=$1
+    if [[ ("$filename" == "") || ("$outputDir" == "") ]]; then
 
-    if [[ ("$filename" == *.gif) && (-f $filename) ]]; then
-        # if [ -d "$FrameDirectory" ]; then
-        #     echo "Clear Frame Dir"
-        #     rm Frames/* # remove all files from Frames and WebMs to prevent errors
-        # else
-        #     mkdir $FrameDirectory
-        # fi
+        echo "Error in input enter: filename OutputDirectoryName"
 
-        rm -rf $outputDir
-        CreateDirectory $outputDir
-        CreateDirectory $outputDir/$FrameDirectory
-
-        # if [ ! -d "$outputDir" ]; then
-        #     mkdir $outputDir
-        # fi
-
-        ffmpeg -i $filename $outputDir/Frames/output_%04d.png 2>NUL # convert video into frames at 30fps, save output files to Frames
-        if [ -e "$NullFile" ]; then
-            rm $outputDir/NUL
-        fi
     else
-        echo "its not a gif file"
+        if [[ ("$filename" == *.gif) && (-f $filename) ]]; then
+
+            rm -rf $outputDir
+            CreateDirectory $outputDir
+            CreateDirectory $outputDir/$FrameDirectory
+
+            ffmpeg -i $filename $outputDir/Frames/output_%04d.png 2>NUL # convert video into frames at 30fps, save output files to Frames
+            RemoveNULFile
+            echo "Done"
+        else
+            echo "its not a gif file"
+        fi
+
     fi
 }
